c549c5fabeb95efe99a47de8b173fc41fa56efb3
mikerr <mike@redrobe.net>
2025-01-03 10:59:44 +0000

    made executable, and added retries

--- wiki.py
+++ wiki.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
 import wikipedia
 
 while True:
-    query = input()
+    query = input(":")
     try: 
         results = wikipedia.summary(query, sentences = 1)
         print(results)
     except:
-        print ("sorry I don't know that")
+        try: # try again 
+            results = wikipedia.summary(query, sentences = 1,features="html.parser")
+            print(results)
+        except:
+            try: 
+                results = wikipedia.summary("what " + query, sentences = 1)
+                print(results)
+            except:
+                print ("sorry I don't know that")
